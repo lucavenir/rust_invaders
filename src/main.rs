e5c7673bb165dfa78886f08b645eae114644882d
venir.dev <luca@venir.dev>
2022-12-23 01:28:20 +0100

    enemies are now floating in front of our face

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crossterm::{
 use rust_invaders::{
     frame::{self, new_frame, Drawable},
     player::Player,
-    render,
+    render, invaders::Invaders,
 };
 use rusty_audio::Audio;
 use std::{error::Error, io, sync::mpsc, thread, time::{Duration, Instant}};
@@ -48,6 +48,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Game Loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
     'gameloop: loop {
         // Per-frame init
         let delta = instant.elapsed();
@@ -76,9 +77,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
 
         // Draw & Render
-        player.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(2));
     }
