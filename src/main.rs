b4187c76c766b8d356b9d6bd80539aa49c4803d3
venir.dev <luca@venir.dev>
2022-12-23 01:46:54 +0100

    added a win / lose condition

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & Render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -88,6 +91,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(2));
+
+        // Win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     // Cleanup
